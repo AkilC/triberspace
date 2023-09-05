fc3b5c68a7b937851e38cba135c90472502915ae
AkilC <cooperakil@gmail.com>
2023-09-05 10:00:57 -0400

    scrolling fixed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-document.addEventListener(
+/* document.addEventListener(
   'touchmove',
   function (event) {
     if (!event.target.classList.contains('allow-scrolling')) {
@@ -15,7 +15,7 @@ document.addEventListener(
     }
   },
   { passive: false }
-);
+); */
 
 
 root.render(
